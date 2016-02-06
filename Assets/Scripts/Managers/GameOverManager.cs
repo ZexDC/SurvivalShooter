5b68c74ba054b574b871a07d535ae93b241eee47
Federico Dalla Rizza <Federico Dalla Rizza>
2016-02-06 21:24:42 +0000

    added gameover screen

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -1,24 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
+    public PlayerHealth playerHealth; // Reference to the player's health.
+    public float restartDelay = 5f;  // Time to wait before restarting the level
 
+    private Animator anim; // Reference to the animator component.
+    private float restartTimer; // Timer to count up to restarting the level
 
-    Animator anim;
-
-
-    void Awake()
+    private void Awake()
     {
+        // Set up the reference.
         anim = GetComponent<Animator>();
     }
 
-
-    void Update()
+    private void Update()
     {
+        // If the player has run out of health...
         if (playerHealth.currentHealth <= 0)
         {
+            // ... tell the animator the game is over.
             anim.SetTrigger("GameOver");
+
+            // .. increment a timer to count up to restarting.
+            restartTimer += Time.deltaTime;
+
+            // .. if it reaches the restart delay...
+            if (restartTimer>restartDelay)
+            {
+                // .. then reload the currently loaded level.
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
         }
     }
 }
